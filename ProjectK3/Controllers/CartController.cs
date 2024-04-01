b48e4c9ec44b1ca9a514e7b3beaa75a21497cfd3
Anhle138795 <ank138795@gmail.com>
2024-04-01 13:23:03 +0700

    12

--- ProjectK3/Controllers/CartController.cs
+++ ProjectK3/Controllers/CartController.cs
@@ -19,10 +19,6 @@ namespace ProjectK3.Controllers
         { 
             return Ok(_cart.Carts.ToList());
         }
-        [HttpPost] Cac
-        public IActionResult GetAll1()
-        {
-            return Ok(_cart.Carts.ToList());
-        }
+        
     }
 }
